7c67b1c78e218d5a909e11a83f3d1092fa279ada
Mikhail  Kritchallo <in-xeper@yandex.ru>
2021-09-18 13:14:24 +0300

    first

---
+++ precache-manifest.df33cb12697999994c3a6f23a6e6e381.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1eeef514f4cba9091d8952eb33701b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "138b419ca72a82ad7f08",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "eca6562c3e8f4e50dcb6",
+    "url": "/static/js/2.51d1ca9a.chunk.js"
+  },
+  {
+    "revision": "138b419ca72a82ad7f08",
+    "url": "/static/js/main.49077348.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
